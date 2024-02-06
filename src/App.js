f06e0837283afe60c0cd9aaa84d597128930bcc1
jspider2023 <153724983+jspider2023@users.noreply.github.com>
2024-02-06 14:31:15 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+ import React from 'react'
+import Immer from './component/Immer'
+import Todo from './component/todo'
+import Counter from './component/Counter'
+import FetchData from './component/FetchData'
+ 
+ function App() {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+   return (
+      <>
+      {/* <Immer/> */}
+      <FetchData/>
+      <Counter/>
+      <hr/>
+      <Todo/>
+      </>
+   )
+ }
+ 
+ export default App
